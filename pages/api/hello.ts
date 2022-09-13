d23e492516c8cd442046383e5692c359b866d01b
Joe Gaebel <joe.gaebel@hotmail.com>
2022-09-13 15:24:19 +1000

    Make it disable, and add Linky

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,18 +9,15 @@ function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const switchSignal = new Gpio(17, 'out');
-switchSignal.writeSync(0);
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
-  const switchSignal = new Gpio(17, 'out');
-    await switchSignal.write(1);
+    res.status(200).json({ name: 'John Doe' })
+    const switchSignal = new Gpio(17, 'out');
+    await switchSignal.write(0);
     console.warn('ON!!!')
     await sleep(2000);
     console.warn('OFF!!!')
-    await switchSignal.write(0);
+    await switchSignal.write(1);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,31 @@
 import type {NextPage} from 'next'
+import {useState} from "react";
+import linky from './linky.png'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
+    const [disabled, setDisabled] = useState(false)
+
     function onClick() {
+        setTimeout(() => setDisabled(false), 2000)
         fetch('/api/hello').then()
+        setDisabled(true)
     }
 
     return (
-        <div>
-            <h1>Linky Garage</h1>
-            <button onClick={onClick}>OPEN!</button>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <h1 style={{marginBottom: '3rem'}}>Linky Garage</h1>
+            <button
+                style={{width: '15rem', height: '5rem'}}
+                onClick={onClick}
+                disabled={disabled}
+            >{disabled ? 'Opening...' : 'OPEN!'}</button>
+            {disabled && <Image src={linky}/>}
         </div>
     )
 }
