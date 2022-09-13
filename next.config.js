f5f6eb4275bbf7413ff4bd35d19d0680ee22010f
Joe Gaebel <joe.gaebel@hotmail.com>
2022-09-13 17:14:29 +1000

    Turn off GPIO at start

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const {Gpio} = require("onoff");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = () => {
+    const switchSignal = new Gpio(17, 'out');
+    switchSignal.writeSync(0);
+    return nextConfig
+}
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,9 +9,6 @@ function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const switchSignal = new Gpio(17, 'out');
-switchSignal.writeSync(0);
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
