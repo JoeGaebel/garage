cadbc4a6e3e36988df3d6162ca5d9510e3f7d115
Joe Gaebel <joe.gaebel@hotmail.com>
2022-09-13 17:04:51 +1000

    Flip the bits to prevent signal leak

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,15 +9,18 @@ function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const switchSignal = new Gpio(17, 'out');
+await switchSignal.write(0);
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
     res.status(200).json({ name: 'John Doe' })
     const switchSignal = new Gpio(17, 'out');
-    await switchSignal.write(0);
+    await switchSignal.write(1);
     console.warn('ON!!!')
     await sleep(2000);
     console.warn('OFF!!!')
-    await switchSignal.write(1);
+    await switchSignal.write(0);
 }
