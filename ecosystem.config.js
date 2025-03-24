c3d6fa2307c0ef75c1a737388e0060fe041de80c
Joe Gaebel <joe@joegaebel.com>
2025-03-24 11:19:39 +1000

    Add node version

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps : [{
     name   : "garage",
     script : "npm run start",
+    interpreter : 'node@22.14.0',
     env: {
       "PASSWORD": "passwordgoeshere",
       "SECRET": "secretgoeshere"
